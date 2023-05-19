94cf0e1db737e8a92ac3af6617c8f7ae2d1d3ec3
Rachana0503 <124668699+Rachana0503@users.noreply.github.com>
2023-05-19 14:05:00 +0530

    Update Demopython_file.py

--- Demopython_file.py
+++ Demopython_file.py
@@ -1,73 +1,62 @@
-from flask import Flask,render_template
 import mysql.connector
+from flask import Flask,render_template
 import csv
-
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="root",
-    database="chikmagalur"
+    password="root"
     )
 mycursor = mydb.cursor()
+#Creating the database and table products
+mycursor.execute("CREATE DATABASE IF NOT EXISTS python")
+mycursor.execute("USE python")
+mycursor.execute("CREATE TABLE IF NOT EXISTS products (product_number VARCHAR(250), date_of_purchase DATE, product_type VARCHAR(250), Price INT)")
 
-#mycursor.execute("CREATE DATABASE monday")
-
-#mycursor.execute("SHOW DATABASES")
-mycursor.execute("SHOW TABLES")
-if mycursor.fetchall():
-    print("already exits")
-else:
-    mycursor.execute("CREATE TABLE products (product_number VARCHAR(250), date_of_purchase DATE, product_type VARCHAR(250), Price INT)")
-#ycursor.execute("CREATE TABLE products (product_number VARCHAR(250), date_of_purchase DATE, product_type VARCHAR(250), Price INT)")
-#mycursor.execute("SHOW TABLES")
+mydb.commit()
 
 
-
-
-file1 = open("rachana.txt","w")
-L = ["RS00122, 2021-06-04, Charger, 40000 \n","RS00222, 2022-07-08, Cable, 6666 \n","RS44003, 2022-01-08, Headset, 40000 \n"]
-#file1.write("product_number, date_of_purchase, product_typr, Price \n")
+#Creating the text file which has product details
+file1 = open("zample.txt","w")
+L = ["MSETC4, 2021-06-04, i-Phone, 490000 \n",
+     "MSETC3, 2022-07-08, hp-laptop, 698766 \n",
+     "MSETAS2, 2021-04-09, CPU, 99000 \n",
+     "MSETAS7, 2021-08-02, Mouse, 900 \n"]
 file1.writelines(L)
 file1.close()
 
 
-with open('rachana.txt', 'r') as in_file:
+
+#Converting the file to csv format
+with open('zample.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line.split(",")for line in stripped if line)
-    with open('rachanamc.csv','w') as out_file:
+    with open('zample.csv','w') as out_file:
         writer = csv.writer(out_file)
         writer.writerow(('product_number','date_of_purchase','product_type','price'))
         writer.writerows(lines)
 
-
-
-csv_data = csv.reader(open('rachanamc.csv'))
-header = next(csv_data)
-#mycursor.execute("INSERT INTO products (product_number, date_of_purchase, product_type, Price) VALUES ('e001', '2021-11-11', 'monitor', '600');")
-print("Importing the file")
-for row in csv_data:
-    if(row):
-        print(row)
-        mycursor.execute("INSERT INTO products (product_number, date_of_purchase, product_type, Price) VALUES (%s, %s, %s, %s);", row)
-mydb.commit()        
-mycursor.close()
-print("Done")
-
 app =Flask(__name__)
-
 @app.route('/')
 def home():
     mycursor = mydb.cursor()
-    
+    csv_data = csv.reader(open('zample.csv'))
+    header = next(csv_data)
+    for row in csv_data:
+        if(row):
+            mycursor.execute("INSERT INTO products(product_number, date_of_purchase, product_type, Price) VALUES (%s, %s, %s, %s);", row)
+    mydb.commit()
+    mycursor.close()
+
+    mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM products")
 
     myresult = mycursor.fetchall()
-    return render_template('sample.html',data=myresult)
+    return render_template('sample2023.html',data=myresult)
 
 if __name__=='__main__':
     app.run(debug=True)
-#for x in myresult:
-  #print(x)
+
+
 
 
 
